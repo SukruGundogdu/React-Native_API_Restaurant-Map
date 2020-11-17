28b4610eb193b4948f5fa5cf9543d2b574623674
sukru <sukrugundogdudatascience@gmail.com>
2020-11-17 19:24:12 +0300

    fit to coordinates option added

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import MapView, {Marker} from 'react-native-maps';
 import Axios from "axios";
 import {SafeAreaView, View, FlatList, Text} from 'react-native';
@@ -10,6 +10,7 @@ let orginalList = [];
 const Main = (props) => {
   const [cityList, setCityList] = useState([]);
   const [restaurants, setRestaurants] = useState([]);
+  const mapRef = useRef(null)
 
 
   const fetchCities = async () => {
@@ -37,6 +38,23 @@ const Main = (props) => {
   conCitySelect = async (city) => {
     const {data: {restaurants} } = await Axios.get("http://opentable.herokuapp.com/api/restaurants?city=" + city)
     setRestaurants(restaurants)
+
+    const restaurantsCoordinates = restaurants.map((res) => {
+      return({
+        latitude: res.lat,
+        longitude: res.lng,
+      });
+    });
+
+    mapRef.current.fitToCoordinates(restaurantsCoordinates, {
+      edgePadding: {
+        top: 50,
+        right: 25,
+        bottom: 25,
+        left: 25
+      }
+    })
+
     console.log(restaurants)
   }
 
@@ -45,12 +63,13 @@ const Main = (props) => {
 
       <View style={{flex: 1}}>
         <MapView
-        style={{flex:1}}
-        initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+          ref={mapRef}
+          style={{flex:1}}
+          initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
       }}>
           {restaurants.map((r, index) => (
     <Marker
@@ -59,8 +78,6 @@ const Main = (props) => {
         latitude: r.lat,
         longitude: r.lng,
       }}
-      title={r.title}
-      description={r.description}
     />
   ))}
       </MapView>
@@ -70,7 +87,9 @@ const Main = (props) => {
         <FlatList
         keyExtractor={(_, index) => index.toString()}
         data={cityList}
-        renderItem={({item}) => <City cityName={item} onSelect={() => conCitySelect(item)}/>}
+        renderItem={({item}) =>(
+          <City cityName={item} onSelect={() => conCitySelect(item)}/>
+          )}
         horizontal
         showsHorizontalScrollIndicator={false}
         />
