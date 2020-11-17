69f2b37db3c73e24f1cc090e2475fad63b02827c
sukru <sukrugundogdudatascience@gmail.com>
2020-11-17 16:30:32 +0300

    Add SearchBar and retrieval API and see cities

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import MapView from 'react-native-maps';
+import Axios from "axios";
+import {SafeAreaView, View, FlatList, Text} from 'react-native';
+
 
 import {City, RestaurantDetail, SearchBar} from './components';
 
 const Main = (props) => {
+  const [cityList, setCityList] = useState([]);
+
+
+  const fetchCities = async () => {
+    const {data} = await Axios.get(
+      "http://opentable.herokuapp.com/api/cities",
+      );
+      setCityList(data.cities);
+  };
+
+  useEffect(() => {
+    fetchCities();
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
+
       <View style={{flex: 1}}>
-            <Text>MAİN</Text>
+        <MapView
+        style={{flex:1}}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+        <View style={{position: "absolute"}}>
+        <SearchBar/>
+        <FlatList
+        keyExtractor={(_, index) => index.toString()}
+        data={cityList}
+        renderItem={({item}) => <Text> {item} </Text>}
+        />
+        </View>
       </View>
     </SafeAreaView>
   );
---
+++ src/styles.js
@@ -0,0 +1,36 @@
+import {Dimensions, StyleSheet} from 'react-native';
+
+export const searchbarStyle = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 5,
+    borderRadius: 10,
+    width: Dimensions.get("window").width * 0.8,
+    alignSelf: 'center',
+    top: 10,
+    left: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    padding: 0,
+    marginLeft: 5
+  },
+});
+
+export const cityStyle = StyleSheet.create({
+  container: {
+  },
+  text: {
+  },
+});
+export const detailStyle = StyleSheet.create({
+  container: {
+  },
+  name: {
+  },
+  address: {
+  },
+  phone: {
+  },
+});
